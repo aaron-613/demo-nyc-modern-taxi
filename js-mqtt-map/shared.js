3e80666482c6d668aad073e285ada550c06cad03
Aaron Lee <github@aaronlee.ca>
2020-08-19 01:43:45 +0800

    adding js mqtt map client

---
+++ js-mqtt-map/shared.js
@@ -0,0 +1,45 @@
+/*
+ This file is used to store the connection credentials, and to initiate an MQTT connection
+*/
+
+  var deets = {
+    host: 'messaging.solace.cloud',
+    port: 8443,
+    ssl: true,
+    username: '',
+    password: ''
+  }
+
+/*
+  // eclipse test server
+  var deets = {
+    host: 'mqtt.eclipse.org',
+    port: 443,
+    ssl: true,
+    username: '',
+    password: '',
+    //path: '/test',
+  }
+*/
+
+  // this is for MQTT, it should return a connected or connecting valid Paho client
+  function getClientConnection(uniqueID,onMessageArrived,onConnectionLost,onConnect) {
+    var client = new Paho.MQTT.Client(deets['host'], Number(deets['port']), uniqueID); // AWS SGP Nano
+    // set the callback handlers
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+    // define connection options
+    var connectOptions = {};
+    if (deets['ssl'] == true) {
+      connectOptions["useSSL"] = true;
+    } else {
+      connectOptions["useSSL"] = false;
+    }
+    //connectOptions["reconnect"] = true;
+    connectOptions["userName"] = deets['username'];
+    connectOptions["password"] = deets['password'];  // AWS SGP Nano
+    connectOptions["onSuccess"] = onConnect;
+    // try to connect!
+    client.connect(connectOptions);
+    return client;
+  }
